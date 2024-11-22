5c6c4dc9cd073d9220395ead88f59a671e421c7a
Okapi-14 <acossardeaux@gaming.tech>
2024-11-22 17:56:10 +0100

    Flappy_Bird_debut

--- Projet_SFML/main.cpp
+++ Projet_SFML/main.cpp
@@ -1,3 +1,24 @@
+#include <SFML/Graphics.hpp>
+
 int main() {
-	return 0;
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "FLAPPY BIRD");
+
+    sf::CircleShape shape(40); 
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(500, 500);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close(); 
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
